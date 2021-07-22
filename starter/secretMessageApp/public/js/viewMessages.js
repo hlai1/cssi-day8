65f9c9db248dcc290dadc4d94ac121934ac23940
Huarui Lai <huarui.lai@gmail.com>
2021-07-22 17:46:13 +0000

    day 9 independent practice w spicy

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,3 +1,26 @@
+const display = (messageObject) => {
+    document.querySelector("#message").innerHTML = messageObject.message;
+    console.log(messageObject);
+};
+
+document.querySelector("#viewMsg").addEventListener("click", (e) => {
+    const userPasscodeGuess = document.querySelector('#passcode').value;
+    const messagesRef = firebase.database().ref();
+    messagesRef.on("value", (snapshot) =>{
+            const data = snapshot.val();
+            for(let key in data){
+                if(data[key].password == userPasscodeGuess){
+                    display(data[key]);
+                }
+            }
+    });
+});
+
+// function display(message){
+//     console.log(message);
+// }
+
+/*
 const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
@@ -32,3 +55,4 @@ document.querySelector("#viewMsg").addEventListener("click", () => {
     const passcode = document.querySelector("#passcode").value;
     findMessage(passcode);
 });
+*/
